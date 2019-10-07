09d49315a4587fa07637179b1a4f66e6458a61cc
Aleks <dreyk.work@list.ru>
2019-10-07 15:38:52 +0300

    First

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strick';
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log (second);
+
+
+let warrior = {
+    name: 'Aizek',
+    age: 89
+    };
+    console.log (warrior ['age']);
+
+
+
+let rumba = ['snec', 'bloom', 'gamble'];
+console.log (rumba [2]);
+
+//alert ('hello!');
+confirm ('you ok?');
+let answer = confirm('motherfucker!');
+console.log (answer); 
